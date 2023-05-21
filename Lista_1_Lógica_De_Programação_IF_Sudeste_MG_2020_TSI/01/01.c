3906aa2b2a41faf0b6b1f05066d1c04a5129b182
M. Fernanda Pinheiro <mfpinheiro02@gmail.com>
2023-05-20 21:32:37 -0300

    Create 01.c
    resolução atividade 01 da lista 01

---
+++ Lista_1_Lógica_De_Programação_IF_Sudeste_MG_2020_TSI/01/01.c
@@ -0,0 +1,27 @@
+/* ENUNCIADO
+
+Dar o tipo de dado de cada uma das constantes:
+
+a) 613
+b) 613.0
+c) –613
+d) “613”
+e) -3.012 * 102
+f) 17*1012
+g) 0.0
+h) “Fim de Questão”
+i) verdadeiro
+j) “falso”
+
+*/
+
+a) int
+b) float
+c) int
+d) string (array char)
+e) double
+f) int
+g) float
+h) string (array char)
+i) bool
+j) string (array char)
