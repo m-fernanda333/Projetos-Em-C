6e601a1b10e1405b3f1d0316ac27120e56d71204
M. Fernanda Pinheiro <mfpinheiro02@gmail.com>
2023-05-20 21:13:36 -0300

    Create HelloWorld.c
    adicionando primeiro arquivo de teste ""hello world"".

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //exibe valores no monitor
+#include <stdlib.h> //permite a alocação de memória
+#include <locale.h> //permite acentuação
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
