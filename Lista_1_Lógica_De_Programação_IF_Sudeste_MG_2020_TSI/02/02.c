1a84ff738a47a5dfa2f78bb21bc11882f1b616ec
M. Fernanda Pinheiro <mfpinheiro02@gmail.com>
2023-05-20 21:46:28 -0300

    Create 02.c
    resolução atividade 02 da lista 02

---
+++ Lista_1_Lógica_De_Programação_IF_Sudeste_MG_2020_TSI/02/02.c
@@ -0,0 +1,26 @@
+/* ENUNCIADO
+
+indique qual o resultado será obtido das seguintes expressões:
+a) 1 / 2
+b) 1 div 2
+c) 1 mod 2
+d) 200 div 10 mod 4
+e) 6 +19-23
+f) 3.0* 5.0 +1
+g) 1/4+2
+h) 28.0/7+4
+i) 3/6.0-7
+
+*/
+
+a) 0 //tendo em vista que são dois inteiros o resultado só poderia ser int
+b) 0
+c) 1
+d) 0
+e) 2
+f) 16.0
+g) 2 //o resultado é 2 pois os numeros são inteiros
+h) 8.0
+i) -6.5
+
+
