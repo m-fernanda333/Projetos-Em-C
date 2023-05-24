c3f988ebe7192e5201bca2007855b5f607b50ea7
M. Fernanda Pinheiro <mfpinheiro02@gmail.com>
2023-05-24 15:00:58 -0300

    QUESTÃO 9 E AJUSTES QUESTÃO 8
    resoluções quase completas

---
+++ Lista_1_Lógica_De_Programação_IF_Sudeste_MG_2020_TSI/09/cabecalho.h
@@ -0,0 +1,10 @@
+#ifndef CABECALHO_H_INCLUDED
+#define CABECALHO_H_INCLUDED
+
+void menuPrincipal();
+int verificaDoubleZero(double valor);
+int verificaIntZero(double valor);
+int verificarEntradaNumerica(char entrada[1000]);
+void limparTela();
+
+#endif // CABECALHO_H_INCLUDED
