0296dc67a02f84e0778ff3e528fe04ac674b1e6f
M. Fernanda Pinheiro <mfpinheiro02@gmail.com>
2023-05-27 23:12:09 -0300

    2º opção de resolução para ex.01 da lista 01

--- Lista_1_Lógica_De_Programação_UNIPAC_MG_2023_Jogos_Digitais/02/main.c
+++ Lista_1_Lógica_De_Programação_UNIPAC_MG_2023_Jogos_Digitais/02/main.c
@@ -11,7 +11,9 @@ valores das variáveis, ou seja, A receba o valor de B e B receba o valor de A
 
 int main(){
 
-    //declarando valores inteiros
+    /* OPÇÃO COM VARIÁVEL TEMPORÁRIA
+
+    //declarando valores inteiros e var. temporaria
     int a = 0, b = 0, temp;
 
     //lendo dois valores inteiros
@@ -20,6 +22,13 @@ int main(){
     printf("\nInforme o valor inteiro B = ");
     scanf("%d", &b);
 
+    //imprimindo valores antes da troca
+    printf("\n ............... \n");
+    printf("\n VALORES ANTES DA TROCA \n\n");
+    printf("\n Valor inteiro A = %d", a);
+    printf("\n Valor inteiro B = %d", b);
+    printf("\n ............... \n");
+
     //trocando os valores
     temp = a; //utilizando uma var. temporária para não perder 1 valor na troca
     a = b;
@@ -27,9 +36,45 @@ int main(){
 
     //imprimindo resultado na tela
     printf("\n ............... \n");
+    printf("\n VALORES DEPOIS DA TROCA \n\n");
     printf("\n Valor inteiro A = %d", a);
     printf("\n Valor inteiro B = %d", b);
     printf("\n ............... \n");
+
+    return 0;
+
+    */
+
+    /* OPÇÃO SEM VARIÁVEL TEMPORÁRIA*/
+
+     //declarando valores inteiros
+    int a = 0, b = 0;
+
+    //lendo dois valores inteiros
+    printf("\nInforme o valor inteiro A = ");
+    scanf("%d", &a);
+    printf("\nInforme o valor inteiro B = ");
+    scanf("%d", &b);
+
+    //imprimindo valores antes da troca
+    printf("\n ............... \n");
+    printf("\n VALORES ANTES DA TROCA: \n\n");
+    printf("\n Valor inteiro A = %d", a);
+    printf("\n Valor inteiro B = %d", b);
+    printf("\n ............... \n\n");
+
+
+    //trocando com operações matemáticas
+    a = a + b; // a = 5 + 3 = 8
+    b = a - b; // b = 8 - 3 = 5
+    a = a - b; // a = 8 - 5 = 3
+
+    //imprimindo resultado na tela
+    printf("\n ............... \n");
+    printf("\n VALORES DEPOIS DA TROCA: \n\n");
+    printf("\n Valor inteiro A = %d", a);
+    printf("\n Valor inteiro B = %d", b);
+    printf("\n ............... \n\n");
 
     return 0;
 }
